91d0c83b375a1eab581314c62e51862a9a92b964
AndyGuit <totalslash@msn.com>
2021-07-02 21:15:17 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const split = expr.match(/.{1,10}/g); // split for words
+  const split2 = split.map(item => item.match(/.{1,2}/g)); // split for morse signals
+
+  let morseCode = '';
+
+  // decode into morse signal
+  split2.forEach(char => {
+    if (char.every(elem => elem == '**')) {
+      morseCode += ' ';
+    };
+    char.forEach(code => {
+      if (code === '11') {
+        morseCode += '-';
+      }
+      if (code === '10') {
+        morseCode += '.';
+      }
+    });
+    morseCode += '|'; // split morse words
+  });
+
+  let result = '';
+
+  // decode morse into word
+  morseCode.split('|').forEach(letter => {
+    if (letter === ' ') {
+      result += ' ';
+    } else if (MORSE_TABLE[letter]) {
+      result += MORSE_TABLE[letter];
+    }
+  });
+
+  return result;
 }
 
 module.exports = {
